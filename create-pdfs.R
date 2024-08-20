a3aea642d2efd113f2426042ad9ea80ea56cddb7
Louisa Smith <louisahsmith@gmail.com>
2024-08-19 21:34:56 -0400

    changes published before day 1

--- create-pdfs.R
+++ create-pdfs.R
@@ -1,11 +1,11 @@
 
-# remotes::install_github("jhelvy/renderthis", dependencies = TRUE)
-list.files("_site/day-1/slides", pattern = "*.html", full.names = TRUE) |>
-  purrr::walk(renderthis::to_pdf)
-# 
-pdftools::pdf_combine(list.files("_site/day-1/slides", pattern = "*.pdf", full.names = TRUE),
-            output = "day-1/slides/day-1-slides.pdf")
-# 
+# # remotes::install_github("jhelvy/renderthis", dependencies = TRUE)
+# list.files("_site/day-1/slides", pattern = "*.html", full.names = TRUE) |>
+#   purrr::walk(renderthis::to_pdf)
+# # 
+# pdftools::pdf_combine(list.files("_site/day-1/slides", pattern = "*.pdf", full.names = TRUE),
+#             output = "day-1/slides/day-1-slides.pdf")
+# # 
 # list.files("_site/day-2/slides", pattern = "*.html", full.names = TRUE) |>
 #   purrr::walk(renderthis::to_pdf)
 # 
